7fc35819d4f2479210069b902ef377a6ab57fadf
devbgandhi <devgandhi1804@gmail.com>
2025-07-19 17:49:52 -0700

    final commit

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
+const screenDisplay = document.querySelector('.screen');
+const buttons = document.querySelectorAll('button');
 
+let calculateArr = [];
+
+function calculate(button) {
+    const val = button.textContent;
+
+    if (button.id === "clear") {
+        screenDisplay.innerText = "0";
+        calculateArr = []; // Clear input
+        return;
+    }
+
+    if (button.id === "equals") {
+        try {
+            const result = eval(calculateArr.join(''));
+            screenDisplay.textContent = result;
+            calculateArr = [result.toString()]; // allow continued calculation
+        } catch (e) {
+            screenDisplay.textContent = "Error";
+            calculateArr = [];
+        }
+        return;
+    }
+
+    // Default: Add value to array and display it
+    calculateArr.push(val);
+    screenDisplay.innerText = calculateArr.join('');
+}
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        console.log(button.innerText);
+        calculate(button);
+    });
+});
